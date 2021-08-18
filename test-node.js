0a25d8ce5cf46eaeb05b2ef8404fdbc49f17da6b
陈建忠 <842289456@qq.com>
2021-08-18 15:53:01 +0800

    first commit

---
+++ test-node.js
@@ -0,0 +1,14 @@
+const MyPromise = require("./MyPromise");
+
+// 测试是否满足Promise A+规范
+MyPromise.deferred = () => {
+  const res = {};
+  const promise = new MyPromise((resolve, reject) => {
+    res.resolve = resolve;
+    res.reject = reject;
+  });
+  res.promise = promise;
+  return res;
+};
+
+module.exports = MyPromise;
